97775732b6425bda54e89fcd9662d25ac35e072f
ROHIT-BHASKAR1 <rohitbhaskar@shooliniuniversity.com>
2025-12-08 21:31:41 +0530

    Create largest_in_array.c

---
+++ largest_in_array.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(){int n,a[100],i,max; scanf("%d",&n); for(i=0;i<n;i++)
+scanf("%d",&a[i]); max=a[0]; for(i=1;i<n;i++) if(a[i]>max) max=a[i];
+printf("%d\n",max); 
+           return 0;}
